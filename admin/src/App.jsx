e27573d83e73e0667391b17e09954443cba0436d
EldorbekDev <eldorbekrajabboyev2007@gmail.com>
2026-07-12 16:33:44 +0500

    Fix admin panel basename for /admin route

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -47,7 +47,7 @@ function Sidebar() {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/admin">
       <div className="flex">
         <Sidebar />
         <div className="flex-1 ml-64">
